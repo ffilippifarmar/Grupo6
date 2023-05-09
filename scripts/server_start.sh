1244c78c7faa509925051342aaffc64bb417dc90
ffilippifarmar <70654853+ffilippifarmar@users.noreply.github.com>
2023-05-09 20:03:24 -0300

    asd

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+cp /home/ec2-user/server/build/libs/*.jar cp /home/ec2-user/server/
 cd /home/ec2-user/server
 sudo java -jar -Dserver.port=80 \
     *.jar > /dev/null 2> /dev/null < /dev/null &
